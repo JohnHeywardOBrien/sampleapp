7fe3d2345f3f51d76ec56ff601ff6ddf24167b9b
John <reachjohnnow@gmail.com>
2015-06-11 19:24:15 +0000

    Working through modeling Users, AR and the like

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611182915) do
+ActiveRecord::Schema.define(version: 20150611185655) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "Name"
+    t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
